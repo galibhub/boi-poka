40b1122c3285e58c4c0f03177b04134a51462325
IBRAHIM AHMED GALIB <ibrahimgalib00@gmail.com>
2025-10-07 13:53:38 +0600

    Implemented sorting according to number of pages and ratings

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -1,40 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import { getStoredBook } from '../../utility/addToDB';
-import Book from '../Book/Book';
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { getStoredBook } from "../../utility/addToDB";
+import Book from "../Book/Book";
 
 const ReadList = () => {
-
-    const [readList,setReadList]=useState([])
-    const data=useLoaderData();
-    useEffect(()=>{
-        const storedBookData=getStoredBook() ;
-        const ConvertedStoredBooks=storedBookData.map(id=>parseInt(id))
-        const myReadList=data.filter(book=>ConvertedStoredBooks.includes(book.bookId))
-        setReadList(myReadList);
-    },[])
-    return (
-        <div>
-            <Tabs>
-    <TabList>
-      <Tab>Read Book List</Tab>
-      <Tab>My WishList</Tab>
-    </TabList>
-
-    <TabPanel>
-      <h2>Book i Read :{readList.length}</h2>
-      {
-        readList.map(b=> <Book key={b.bookId} singleBook={b}></Book>)
-      }
-    </TabPanel>
-    <TabPanel>
-      <h2>My WishList</h2>
-    </TabPanel>
-  </Tabs>
-        </div>
+  const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
+  const data = useLoaderData();
+  useEffect(() => {
+    const storedBookData = getStoredBook();
+    const ConvertedStoredBooks = storedBookData.map((id) => parseInt(id));
+    const myReadList = data.filter((book) =>
+      ConvertedStoredBooks.includes(book.bookId)
     );
+    setReadList(myReadList);
+  }, []);
+
+  const handleSort = (type) => {
+    setSort(type);
+    if (type === "pages") {
+      const sortedByPage = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages
+      );
+      setReadList(sortedByPage);
+    }
+    if (type === "ratings") {
+        const sortedByrating=[...readList].sort((a,b)=>a.rating-b.rating)
+        setReadList(sortedByrating);
+    }
+  };
+
+  return (
+    <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by: {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("ratings")}>rating</a>
+          </li>
+        </ul>
+      </details>
+      <Tabs>
+        <TabList>
+          <Tab>Read Book List</Tab>
+          <Tab>My WishList</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Book i Read :{readList.length}</h2>
+          {readList.map((b) => (
+            <Book key={b.bookId} singleBook={b}></Book>
+          ))}
+        </TabPanel>
+        <TabPanel>
+          <h2>My WishList</h2>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ReadList;
