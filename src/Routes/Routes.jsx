7115b3842fc4555196f5dfc90561a74acfc41bfa
IBRAHIM AHMED GALIB <ibrahimgalib00@gmail.com>
2025-10-07 13:37:05 +0600

    Implement ReadList Functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -5,6 +5,7 @@ import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import BookDetails from '../pages/BookDetails/BookDetails';
+import ReadList from '../pages/ReadList/ReadList';
 
 export const router = createBrowserRouter([
   {
@@ -22,11 +23,19 @@ export const router = createBrowserRouter([
         path:'/about',
         Component:About
     },
+
+  {
+        path:'readList',
+        Component:ReadList,
+        loader:()=>fetch('/booksData.json'),
+    },
+
     {
         path:'/bookDetails/:id',
         loader:()=>fetch('/booksData.json'),
         Component:BookDetails
     }
+  
    ]
   },
 ]);
